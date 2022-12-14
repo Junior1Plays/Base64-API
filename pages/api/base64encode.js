347cd858899e296539d561db676a69e8682877ba
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2022-12-14 02:33:04 -0300

    Update base64encode.js

--- pages/api/base64encode.js
+++ pages/api/base64encode.js
@@ -1,5 +1,6 @@
 async function base64encode(request, response) {
     const string = request.query.string;
-    return response.status(200).send(btoa(string));
+    if(!string) return response.send("Missing string content.")
+    return response.status(200).send(atob(string));
 }
 export default base64encode;
