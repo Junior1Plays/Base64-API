827717d71104c353077b157b0337f3df6310cbe4
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2022-12-14 02:40:08 -0300

    Update base64encode.js

--- pages/api/base64encode.js
+++ pages/api/base64encode.js
@@ -2,7 +2,7 @@ async function base64encode(request, response) {
     const string = request.query.string;
     try {
         let resultado = atob(string);
-        return response.send(string);
+        return response.send(resultado);
     } catch {
         return response.send("Error converting text to Base64.");
     }
