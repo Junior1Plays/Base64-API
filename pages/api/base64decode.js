5e8cfc0565715af592dbdd85accf58c9868e22ff
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2022-12-14 02:35:02 -0300

    Update base64decode.js

--- pages/api/base64decode.js
+++ pages/api/base64decode.js
@@ -1,6 +1,5 @@
 async function base64decode(request, response) {
     const string = request.query.string;
-    if(!string) return response.send("Missing string content.")
     return response.status(200).send(btoa(string));
 }
 export default base64decode;
