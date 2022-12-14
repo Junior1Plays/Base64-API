82291cf0efde0b69e6693a0ca67a19da82a6967a
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2022-12-14 02:40:26 -0300

    Update base64decode.js

--- pages/api/base64decode.js
+++ pages/api/base64decode.js
@@ -2,7 +2,7 @@ async function base64decode(request, response) {
     const string = request.query.string;
     try {
         let resultado = btoa(string);
-        return response.send(string);
+        return response.send(resultado);
     } catch {
         return response.send("Error converting Base64 to text.");
     }
